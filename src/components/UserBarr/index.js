1be5cbf4e252c0dc6f58ceb5fa604ed41b6f7899
aghasi89 <59113908+EdgarGyumri@users.noreply.github.com>
2022-03-27 15:39:05 +0400

    same think for home work

--- src/components/UserBarr/index.js
+++ src/components/UserBarr/index.js
@@ -1,13 +1,20 @@
 import vector from '../../asete/imagines/Vector.png';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import HidenMenu from '../HidenMenu';
 import './style.css';
+import DOCTORS from '../../data/doctors';
 
 
 export default (props) => {
   const [menuStatus, setMenuStatus] = useState(true);
   const { name } = props;
   const initial = name.split("")[0];
+  const [type,setType] = useState();
+  // սա պետք է օգտագործել DoctorsDetails view ի մեջը
+  //DOCTORS.find((item=>item.id===id))
+  const doctros  = useMemo(()=>{
+    return DOCTORS.filter((item=>item.type===type))
+  },[type])
   function menuHandler() {
     setMenuStatus(!menuStatus);
   }
@@ -17,6 +24,9 @@ export default (props) => {
       <span className="avatar-name">{name}</span>
       <img src={vector} alr="vecror" className="vector" onClick={menuHandler} />
       {menuStatus ? <HidenMenu /> : null}
+      {doctros.map(item=>{
+        return <Link to={`/doctorsDetails/${iten.id}`}></Link>
+      })}
     </div>
   )
 };
---
+++ src/data/doctors.js
@@ -0,0 +1,80 @@
+const DOCTORS = [
+    {
+        id:1,
+        type:"",
+        name:"POghos",
+        description:"",
+        experince:4,
+        educations:[
+            {
+                title:"",
+                when:"2010/06/14"
+            },
+            {
+                title:"",
+                when:"2012/06/14"
+            }
+        ],
+
+
+    },
+    {
+        id:1,
+        type:"",
+        name:"POghos",
+        description:"",
+        experince:4,
+        educations:[
+            {
+                title:"",
+                when:"2010/06/14"
+            },
+            {
+                title:"",
+                when:"2012/06/14"
+            }
+        ],
+
+
+    },
+    {
+        id:1,
+        type:"",
+        name:"POghos",
+        description:"",
+        experince:4,
+        educations:[
+            {
+                title:"",
+                when:"2010/06/14"
+            },
+            {
+                title:"",
+                when:"2012/06/14"
+            }
+        ],
+
+
+    },
+    {
+        id:1,
+        type:"",
+        name:"POghos",
+        description:"",
+        experince:4,
+        educations:[
+            {
+                title:"",
+                when:"2010/06/14"
+            },
+            {
+                title:"",
+                when:"2012/06/14"
+            }
+        ],
+
+
+    }
+]
+
+export default DOCTORS
